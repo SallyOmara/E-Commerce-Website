e8b9441f257be7f33bc26e7409b32990a10f56f8
SallyOmara <sallyahmedomara12@gmail.com>
2025-09-27 00:14:55 +0300

    fix build issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // dynamic routes → use SSR
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
+
+  // everything else → prerender
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
